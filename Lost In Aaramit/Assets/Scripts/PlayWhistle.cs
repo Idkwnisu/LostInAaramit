1557f806ebbbf5a939787dbda13e6e1e8a6badca
Alessandro Carughi <10455590@polimi.it>
2018-11-06 16:08:47 +0100

    MazeScene
    MazeScene with 3D sound

---
+++ Lost In Aaramit/Assets/Scripts/PlayWhistle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayWhistle : MonoBehaviour {
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    public int timer;
+    AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+
+        StartCoroutine(PlaySound());
+    }
+
+
+    IEnumerator PlaySound()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timer);
+            audio.PlayOneShot(SoundToPlay, Volume);
+        }
+    }
+}
