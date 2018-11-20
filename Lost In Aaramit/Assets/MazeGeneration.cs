f00ea403d3b66e1320a105de8205c56ec85fe1a3
Idkwnisu <stefano-palma@hotmail.it>
2018-11-20 12:10:11 +0100

    Added maze generation from image scene

---
+++ Lost In Aaramit/Assets/MazeGeneration.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGeneration : MonoBehaviour {
+    public Texture2D text;
+    public GameObject prefab;
+    public float ScalePrefab;
+    private Mesh _mesh;
+   
+
+    // Use this for initialization
+    void Start () {
+        _mesh = prefab.GetComponentInChildren<MeshFilter>().sharedMesh;
+        float _meshH = _mesh.bounds.size.x * prefab.transform.localScale.x * ScalePrefab;
+        float _meshW = _mesh.bounds.size.z * prefab.transform.localScale.z * ScalePrefab;
+        for (int x = 0; x < text.width; x++)
+        {
+            for (int y = 0; y < text.height; y++)
+            {
+                if (text.GetPixel(x, y) == Color.black)
+                {
+                    Vector3 position = new Vector3(transform.position.x + _meshH * x, transform.position.y, transform.position.z + _meshW * y);
+                    GameObject cube = Instantiate(prefab, position, transform.rotation);
+                    cube.transform.parent = gameObject.transform;
+                    cube.transform.localScale = cube.transform.localScale * ScalePrefab;
+                }
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
