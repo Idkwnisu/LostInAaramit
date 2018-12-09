01d66fa1a4f3987c9a9bc4c42b363766a0f1f3b0
marco-amoruso <marco1.amoruso@mail.polimi.it>
2018-12-09 17:02:04 +0100

    Selection scene menu

---
+++ Lost In Aaramit/Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelection : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void SelectScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SceneManager.LoadScene("SceneSelectionMenu", LoadSceneMode.Single);
+        }
+    }
+}
