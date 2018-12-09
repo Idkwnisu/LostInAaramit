28110ecb1755f3baf262ce07f58c99780b5a0e27
Alessandro Carughi <10455590@polimi.it>
2018-12-09 11:53:50 +0100

    Add CheckPoint

---
+++ Lost In Aaramit/Assets/Scripts/ChangeCheckPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCheckPoint : MonoBehaviour {
+
+    public GameObject checkPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(checkPoint);
+        }
+    }
+}
---
+++ Lost In Aaramit/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    public Transform checkPoint;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")){
+            player.transform.position = checkPoint.position;
+            player.transform.rotation = checkPoint.rotation;
+        }
+    }
+}
