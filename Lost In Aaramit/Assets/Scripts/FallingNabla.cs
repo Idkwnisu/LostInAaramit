21f5e56ccbfbf99bb39a093d335dd86dfb2495f6
Alessandro Carughi <10455590@polimi.it>
2018-12-13 17:04:32 +0100

    Update with Nature

--- Lost In Aaramit/Assets/Scripts/FallingNabla.cs
+++ Lost In Aaramit/Assets/Scripts/FallingNabla.cs
@@ -14,7 +14,7 @@ public class FallingNabla : MonoBehaviour {
 
     IEnumerator FallNabla(GameObject nabla)
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
         nabla.GetComponent<Rigidbody>().isKinematic = false;
     }
 
