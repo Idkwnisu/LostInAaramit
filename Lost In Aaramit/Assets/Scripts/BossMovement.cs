265276572c59682c0d92351e177534a36579a435
Alessandro Carughi <10455590@polimi.it>
2019-01-06 11:25:10 +0100

    BossScene

--- Lost In Aaramit/Assets/Scripts/BossMovement.cs
+++ Lost In Aaramit/Assets/Scripts/BossMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BossMovement : MonoBehaviour {
 
+    public GameObject Boss;
     public Transform[] target;
     private float v = 4f;
 
@@ -15,16 +16,17 @@ public class BossMovement : MonoBehaviour {
 	private void Update () {
         if(transform.position != target[c].position){
             Vector3 pos = Vector3.MoveTowards(transform.position, target[c].position, v * Time.deltaTime);
+            Vector3 posB = pos + new Vector3(0, 1f, 0);
             GetComponent<Rigidbody>().MovePosition(pos);
+            Boss.GetComponent<Rigidbody>().MovePosition(posB);
         } else {
             c = (c + 1) % target.Length;
         }
 	}
 
-    public void setActive(float speed){
+    public void setActive(float speed) {
         enabled = true;
         v = speed;
-
     }
 
     public void setNotActive()
