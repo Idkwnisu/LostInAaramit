62cee2ea8e4eacc770bb4f44129c9728545ef88d
Idkwnisu <stefano-palma@hotmail.it>
2019-01-21 16:13:37 +0100

    Added collectibles scripts

---
+++ Lost In Aaramit/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{
+    public float speed = 100;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,speed*Time.deltaTime,0));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CollectiblesManager.instance.addPoint();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Lost In Aaramit/Assets/Scripts/CollectiblesManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblesManager : MonoBehaviour {
+
+    public static CollectiblesManager instance = null;
+
+    private int collectibles = 0;
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+            instance = this;
+
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(this);
+    }
+
+    public void addPoint()
+    {
+        collectibles += 1;
+        Debug.Log("Now you have " + collectibles + " points");
+    }
+
+    public int howManyPoints()
+    {
+        return collectibles;
+    }
+
+    public bool spendPoints(int points)
+    {
+        if (collectibles >= points)
+        {
+            collectibles -= points;
+            return true;
+        }
+        return false;
+    }
+}
