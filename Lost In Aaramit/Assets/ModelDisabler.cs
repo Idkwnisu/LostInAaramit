bd4ef95c43de4393868b9bf74ab9e7332156da8d
Idkwnisu <stefano-palma@hotmail.it>
2019-02-20 19:21:21 +0100

    Lag moderator added

---
+++ Lost In Aaramit/Assets/ModelDisabler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelDisabler : MonoBehaviour {
+    public GameObject[] toDisable;
+    public float distance;
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        Invoke("Check", 2.0f);
+	}
+
+    private void Check()
+    {
+        for (int i = 0; i < toDisable.Length; i++)
+        {
+			Debug.Log(player.transform.position);
+            //Debug.Log(Vector3.Distance(player.transform.position, toDisable[i].transform.position));
+            if (Vector3.Distance(player.transform.position, toDisable[i].transform.position) > distance)
+            {
+                toDisable[i].SetActive(false);
+            }
+            else
+            {
+                toDisable[i].SetActive(true);
+            }
+        }
+        Invoke("Check", 2.0f);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
